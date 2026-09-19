338b50708e941ce1c7e1f3985f4ff6673339b934
sabren emad <Sabren.Emad.Tech@gmail.com>
2026-09-19 12:55:17 +0300

    update all files

--- js.js
+++ js.js
@@ -26,8 +26,19 @@ total.innerHTML = result;
     }
 }
 
-let dataPro = [];
-submit.onclick = function(){
+let dataPro;
+
+if(localStorage.getItem("product") != null){
+
+dataPro = JSON.parse(localStorage.getItem("product"));
+}
+else{
+
+ dataPro = [];
+}
+
+
+ submit.onclick = function(){
 
     let newPro ={
     title: title.value,
@@ -43,13 +54,20 @@ submit.onclick = function(){
 
     if(title.value !="" && price.value !="" &&  category.value !=""){
 
+ 
+        for(let y = 0; y < count.value; y++){
+            
         dataPro.push(newPro);
+        }
+
         clearData();
 
-        localStorage.setItme("product" , JSON.stringify(dataPro));
-    }
+        localStorage.setItem("product" , JSON.stringify(dataPro));
 
-}
+        showData();
+
+    }
+ }
 
 function clearData(){
 
@@ -61,5 +79,62 @@ price.value ='';
  total.innerHTML = '';
 count.value = '';
 category.value ='';
+
+}
+
+
+function showData(){
+
+let table ="";
+
+for(let x = 0; x < dataPro.length; x++){
+
+ table += `
+ 
+ <tr>
+
+<td>${x + 1}</td>
+<td>${dataPro[x].title}</td>
+<td>${dataPro[x].price}</td>
+<td>${dataPro[x].taxes}</td>
+<td>${dataPro[x].ads}</td>
+<td>${dataPro[x].discount}</td>
+<td>${dataPro[x].total}</td>
+<td>${dataPro[x].category}</td>
+<td> <button id="update">UPDATE</button></td>
+<td><button onclick = "deleteData (${x}) "id="delete">DELETE</button></td>
+
+ </tr>
+
+ 
+ `
+}
+
+document.getElementById("tbody").innerHTML = table ;
+
+document.getElementById("countProduct").innerHTML = dataPro.length;
+
+
+}  showData();
+
+
+function deleteData(i){
+
+dataPro.splice(i, 1);
+
+localStorage.setItem("product" , JSON.stringify(dataPro));
+
+ 
+
+showData();
+}
+
+
+function deleteAll(){
+
+dataPro.splice(0, dataPro.length);
+localStorage.setItem("product" , JSON.stringify(dataPro));
+ 
+ showData();
 
 }
